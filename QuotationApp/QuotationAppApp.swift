2207dea07939b82e45f19018b4e8689dc542360c
Eiphoria <liuerbsb@gmal.com>
2021-03-12 12:37:16 +0900

    Initial Commit

---
+++ QuotationApp/QuotationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotationAppApp.swift
+//  QuotationApp
+//
+//  Created by LIU SHURUI on 2021/03/12.
+//
+
+import SwiftUI
+
+@main
+struct QuotationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
